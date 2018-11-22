6576d41d5b30638d2a560219943167cae10cf6b9
DSLee <bryan.lee@linecorp.com>
2018-11-22 16:23:36 +0900

    usage of 'readLine'

--- Sources/SwiftConsole/main.swift
+++ Sources/SwiftConsole/main.swift
@@ -1,3 +1,11 @@
 import Foundation
 
+import CommandLineKit
+import Swiftline
+
 print("Hello, world!")
+print(CommandLine.arguments)
+
+guard let input = readLine() else { exit(0) }
+print(input)
+
